a67c9b434a0b9fb7a959d5f0cc20c846e21114c5
tharunyagavusegithub <tharunyagav@gmail.com>
2025-01-27 19:23:53 +0530

    order update

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,4 +1,7 @@
 const express=require('express');
 const route= express.Router();
+const {createOrder} =require('../controller/orderController');
 
-router.route('/order'). post(getproducts);
+router.route('/order'). post(createOrder);
+
+module.exports= router;
