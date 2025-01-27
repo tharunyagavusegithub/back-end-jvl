cf05a35982054165064cbcd479ae898edd91617d
tharunyagavusegithub <tharunyagav@gmail.com>
2025-01-27 19:23:32 +0530

    productcontrol update

---
+++ backend/controller/productController.js
@@ -0,0 +1,12 @@
+exports.getproducts =(req,res,next) =>{
+    res.json({
+        success:true,
+        Message:'get products working!'
+    })
+}
+exports.getSingleproduct =(req,res,next) =>{
+    res.json({
+        success:true,
+        Message:'get Single product working!'
+    })
+}
