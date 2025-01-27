4aa82c5b4ef163b1265cad28ae4a8ecacc9abd40
tharunyagavusegithub <tharunyagav@gmail.com>
2025-01-27 19:25:33 +0530

    update

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,9 @@
 const express=require('express');
-const { getproducts } = require('./controller/productController');
+const { getproducts, getSingleproduct } = require('../controller/productController');
 const route= express.Router();
 
 router.route('/products').get(getproducts);
-router.route('/product/id').get(getproducts);
+router.route('/product/id').get(getSingleproduct);
 
 module.exports =router;
 
